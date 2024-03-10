4c513ecb473201996a259ce8454244610f7b2558
jeromecabugwason <jeromecabugwason2@protonmail.com>
2024-03-10 16:17:24 +0800

    add fast collinear points usage

--- src/main/java/com/zireline/collinearpoints/Renderer.java
+++ src/main/java/com/zireline/collinearpoints/Renderer.java
@@ -2,17 +2,27 @@ package com.zireline.collinearpoints;
 
 import java.util.List;
 
+// import com.zireline.collinearpoints.algorithm.FastCollinearPoints;
 import com.zireline.collinearpoints.display.GridPlane;
 
 public class Renderer {
   public static void render(GridPlane root, List<Point> points) {
+    // USAGE
+    // FastCollinearPoints fastCollinearPoints = new FastCollinearPoints(points);
 
-    for (int i = 0; i < points.size() - 1; i++) {
-      Point currentPoint = points.get(i);
-      Point nextPoint = points.get(i + 1);
-      root.drawShape(currentPoint.drawTo(nextPoint));
-      root.drawShape(currentPoint);
-      System.out.println("Drew point: " + currentPoint + "to " + nextPoint);
+    // for (LineSegment segment : fastCollinearPoints.segments()) {
+    // System.out.println(segment.toString());
+    // root.drawShape(segment);
+    // }
+
+    Point previousPoint = null;
+    for (Point currentPoint : points) {
+      if (previousPoint != null) {
+        root.drawShape(previousPoint.drawTo(currentPoint));
+        root.drawShape(previousPoint);
+        System.out.println("Drew point: " + previousPoint + "to " + currentPoint);
+      }
+      previousPoint = currentPoint;
     }
   }
 }
--- src/main/java/com/zireline/collinearpoints/algorithm/FastCollinearPoints.java
+++ src/main/java/com/zireline/collinearpoints/algorithm/FastCollinearPoints.java
@@ -1,9 +1,12 @@
 package com.zireline.collinearpoints.algorithm;
 
+import java.util.List;
+
 import com.zireline.collinearpoints.LineSegment;
+import com.zireline.collinearpoints.Point;
 
 public class FastCollinearPoints {
-  public FastCollinearPoints() {
+  public FastCollinearPoints(List<Point> points) {
     // default constructor
   }
 
