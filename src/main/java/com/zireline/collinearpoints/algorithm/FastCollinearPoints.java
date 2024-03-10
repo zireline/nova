b4f0d8156e46ebc34c127a4cc6d2f515cd78d69d
jeromecabugwason <jeromecabugwason2@protonmail.com>
2024-03-10 15:09:03 +0800

    add fast collinear points class

---
+++ src/main/java/com/zireline/collinearpoints/algorithm/FastCollinearPoints.java
@@ -0,0 +1,17 @@
+package com.zireline.collinearpoints.algorithm;
+
+import com.zireline.collinearpoints.LineSegment;
+
+public class FastCollinearPoints {
+  public FastCollinearPoints() {
+    // default constructor
+  }
+
+  public int numberOfSegments() {
+    return 0;
+  }
+
+  public LineSegment[] segments() {
+    return null;
+  }
+}
